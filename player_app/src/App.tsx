6387c3158bc2050062cef1c9db4df9a2f7510bfa
Andrew Fleming <ajfleming1992@gmail.com>
2020-11-20 17:08:03 +0000

    Setup OnsenUI and removed all the compiler warnings

--- player_app/src/App.tsx
+++ player_app/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import 'onsenui/css/onsenui.css';
+import 'onsenui/css/onsen-css-components.css';
 import { ViewState } from "./redux/reducers/view";
 import Home from "./pages/Home";
 import { connect } from "react-redux";
--- player_app/src/pages/Home.tsx
+++ player_app/src/pages/Home.tsx
@@ -1,10 +1,28 @@
 import React from "react";
-import { Page } from "react-onsenui";
+import { Input, Page, Button } from "react-onsenui";
+import { connect } from "react-redux";
+import './Home.css';
 
-const Home = () => (
-  <Page material>
+interface HomeProps {
 
+}
+
+const Home: React.FC<HomeProps> = (props) => (
+  <Page>
+    <div className="header">
+      <h1>Two Rooms and a BOOM</h1>
+    </div>
+    <div>
+      <Input className='input' placeholder="Name"/>
+      <Button modifier="large--cta">Testing</Button>
+    </div>
   </Page>
 );
 
-export default Home;
+const mapStateToProps = (state: any) => {
+    return {
+
+    }
+}
+
+export default connect(mapStateToProps)(Home);
