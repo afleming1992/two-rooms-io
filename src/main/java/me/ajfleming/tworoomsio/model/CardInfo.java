9c0e1f19ae5d43b6477dcbec6f17cc8a77f897dd
Andrew Fleming <ajfleming1992@gmail.com>
2020-12-29 15:30:22 +0000

    Added new Card Property for Subtitle to give more inforamtion to the FrontEnd (so it can make better cards)

--- src/main/java/me/ajfleming/tworoomsio/model/CardInfo.java
+++ src/main/java/me/ajfleming/tworoomsio/model/CardInfo.java
@@ -5,29 +5,38 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum CardInfo {
 	PRESIDENT( "President",
+			"Avoid the Bomber",
 			"You are a primary character. The blue team wins if you do not gain the 'dead' condition",
 			"president_blue", Team.BLUE ),
 	BOMBER( "Bomber",
+				"Be with the President",
 			"You are a primary character. Everyone in the same room as you at the end of the game dies! The Red Team wins if the president dies!",
 			"bomber_red", Team.RED ),
 	GAMBLER( "Gambler",
+				"Guess if Red, Blue or Neither team won",
 			"At the end of the last round, you will publically announce which team you think won the game. You only win if you are correct",
 			"gambler", Team.GREY ),
-	BLUE_TEAM( "Blue Team", "You are on the blue team. You win if the president doesn't die",
+	BLUE_TEAM( "Blue Team",
+			"Keep the President away from the Bomber",
+			"You are on the blue team. You win if the president doesn't die",
 			"blue", Team.BLUE ),
-	RED_TEAM( "Red Team", "You are on the red team. You win if the president dies!", "red",
+	RED_TEAM( "Red Team",
+			"Get the Bomber to be with the President",
+			"You are on the red team. You win if the president dies!", "red",
 			Team.RED );
 
 	private final String title;
+	private final String subtitle;
 	private final String howToWin;
 	private final String cardImage;
 	private final Team team;
 
-	CardInfo( String title, String howToWin, String cardImage, Team team ) {
+	CardInfo( String title, String subtitle, String howToWin, String cardImage, Team team ) {
 		this.title = title;
 		this.howToWin = howToWin;
 		this.cardImage = cardImage;
 		this.team = team;
+		this.subtitle = subtitle;
 	}
 
 	public String getTitle() {
@@ -44,5 +53,9 @@ public enum CardInfo {
 
 	public Team getTeam() {
 		return team;
+	}
+
+	public String getSubtitle() {
+		return subtitle;
 	}
 }
