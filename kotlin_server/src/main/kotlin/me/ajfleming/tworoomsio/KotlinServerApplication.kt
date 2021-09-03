63678328252f34c69006e718cc108f493828bd43
andrewfleming <andrew.fleming@fanduel.com>
2021-09-03 14:43:25 +0100

    Add Kotlin Server folder and setup Spring Boot Application

---
+++ kotlin_server/src/main/kotlin/me/ajfleming/tworoomsio/KotlinServerApplication.kt
@@ -0,0 +1,11 @@
+package me.ajfleming.tworoomsio
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinServerApplication>(*args)
+}
---
+++ kotlin_server/src/test/kotlin/me/ajfleming/tworoomsio/KotlinServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.ajfleming.tworoomsio
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
